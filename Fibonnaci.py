07303ae9d033dbaa5ba79ed8b5ee9676d170a082
Saiesh Savant <saieshsawant70@gmail.com>
2023-12-19 11:21:05 +0530

    gs

--- Fibonnaci.py
+++ Fibonnaci.py
@@ -9,6 +9,7 @@ n =int(input("Enter the no:"))
 result = fibonacci(n)
 print(f"The first {n} numbers in the Fibonacci series are: {result}")
 '''
+Hello
 Enter the no:10
 The first 10 numbers in the Fibonacci series are: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
 '''
