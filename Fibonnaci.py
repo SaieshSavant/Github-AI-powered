f35af47b93b6d8be02db4e5cc1f3c1a4637b889f
Saiesh Savant <saieshsawant70@gmail.com>
2024-02-27 22:46:29 +0530

    gg

--- Fibonnaci.py
+++ Fibonnaci.py
@@ -3,7 +3,7 @@ def fibonacci(n):
     while len(fib_series) < n:
         fib_series.append(fib_series[-1] + fib_series[-2])
     return fib_series
-
+#hello
 # Example: Generate the first 10 Fibonacci numbers
 n =int(input("Enter the no:"))
 result = fibonacci(n)
